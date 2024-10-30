a95b76e4ff73e6ff4dc401c94b5e19431a26b6c5
reikasar <183247743+reikasar@users.noreply.github.com>
2024-10-30 21:35:06 +0200

    add solution for excerise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+  if (year % 4 == 0) {
+    if (year % 100 != 0) return true;
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) return true;
+    else return false;
+  }
+  else if (year % 100 == 0 && year % 400 == 0) return true;
+  else return false;
+}  
+//console.log(leapYears(1985))
 
 // Do not edit below this line
 module.exports = leapYears;
