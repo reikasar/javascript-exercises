78052295bd9cbb21e01f4e05cd106b7058c196e8
reikasar <183247743+reikasar@users.noreply.github.com>
2024-11-22 19:53:21 +0200

    add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+  const fib = [0, 1];
+  if (n >= 0) {
+    for (let i = 2; i <= n; i++) {
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[n];
+  } else {
+    return "OOPS"
+  }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
